a047b3b3070d80c7c870c776f0f4419760ea1dfa
Evelina <evelina.tev@mail.ru>
2024-08-25 14:48:51 +0300

    Handlers implemented

---
+++ src/main/java/handlers/Handlers.java
@@ -0,0 +1,10 @@
+package handlers;
+
+
+import java.io.BufferedOutputStream;
+import java.io.IOException;
+
+@FunctionalInterface
+public interface Handlers {
+    void handle(Request request, BufferedOutputStream responseStream) throws IOException;
+}
