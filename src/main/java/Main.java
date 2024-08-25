c6b81927d129cb932ba91d952fa14aa3b062b671
Evelina <evelina.tev@mail.ru>
2024-08-25 14:57:00 +0300

    Main implemented

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,23 @@
-public class Main {
-    public static final int SERVER_SOCKET = 9999;
-    public static final int POOL_SIZE = 64;
+import java.io.*;
 
-    public static void main(String[] args) {
+public class Main {
+    private static final int SERVER_SOCKET = 9999;
+    private static final int POOL_SIZE = 64;
+
+    public static void main(String[] args) throws InterruptedException {
         Server server = new Server(SERVER_SOCKET, POOL_SIZE);
-        server.start();
+        server.addHandler("GET", "/messages", ((request, responseStream) -> {
+            try {
+                server.responseWithoutContent(responseStream, "404", "Not found");
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }));
+
+        server.addHandler("POST", "/messages", (request, responseStream) -> server.responseWithoutContent(responseStream,
+                "404", "Not found"));
+        server.listen();
+
+
     }
 }
